b016a392a851d0e6e9d72e9696093786f62b2ebe
npsfilm <82311300+npsfilm@users.noreply.github.com>
2026-01-30 17:03:25 +0100

    Add PdfDocument component for rendering templates

---
+++ src/PdfDocument.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { ImmobilienTemplate } from "./templates/ImmobilienTemplate";
+import { DrohnenTemplate } from "./templates/DrohnenTemplate";
+import { KombiTemplate } from "./templates/KombiTemplate";
+import { StagingTemplate } from "./templates/StagingTemplate";
+import type { PdfData } from "./types";
+
+interface Props {
+  data: PdfData;
+}
+
+export const PdfDocument: React.FC<Props> = ({ data }) => {
+  const shootingType = data.project.shootingType.toLowerCase();
+  
+  if (shootingType.includes("drohne") && !shootingType.includes("kombi")) {
+    return <DrohnenTemplate data={data} />;
+  }
+  
+  if (shootingType.includes("kombi")) {
+    return <KombiTemplate data={data} />;
+  }
+  
+  if (shootingType.includes("staging") || shootingType.includes("virtuell")) {
+    return <StagingTemplate data={data} />;
+  }
+  
+  // Default: Immobilien
+  return <ImmobilienTemplate data={data} />;
+};
